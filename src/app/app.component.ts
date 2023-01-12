ffc719fc78115a7a2c6d78b9bf50f68bb8a4e262
murillo.nahas <murillonahas@hotmail.com>
2023-01-12 11:52:29 -0300

    feat: creating header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
-  template: `<router-outlet></router-outlet>`,
+  template: ` <main class="wrapper">
+    <app-header></app-header>
+    <router-outlet></router-outlet>
+  </main>`,
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {}
