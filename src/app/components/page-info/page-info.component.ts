df85cc2a14a7a8bd540d1b4f6d91e937a62bcb46
murillo.nahas <murillonahas@hotmail.com>
2023-01-12 15:32:20 -0300

    feat: changing selector component

--- src/app/components/page-info/page-info.component.ts
+++ src/app/components/page-info/page-info.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-page-info',
+  selector: 'page-info',
   templateUrl: './page-info.component.html',
   styleUrls: ['./page-info.component.scss'],
 })
