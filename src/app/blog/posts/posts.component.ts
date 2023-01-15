7dc6beb093157336f3b5bb191f49adb9626d11ef
murillo.nahas <murillonahas@hotmail.com>
2023-01-15 16:35:44 -0300

    style: removing code

--- src/app/blog/posts/posts.component.ts
+++ src/app/blog/posts/posts.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.scss']
+  styleUrls: ['./posts.component.scss'],
 })
-export class PostsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class PostsComponent {}
