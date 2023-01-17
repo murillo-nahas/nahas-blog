fc6e797684d1f251dda61bf6335480fdcaf4e9a7
murillo.nahas <murillonahas@hotmail.com>
2023-01-17 14:45:20 -0300

    feat: some changes

--- src/app/components/shared-components.module.ts
+++ src/app/components/shared-components.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PageInfoComponent } from './page-info/page-info.component';
 import { HomePostBlockComponent } from './home-post-block/home-post-block.component';
+import { PostInformationComponent } from './post-information/post-information.component';
+import { ReturnToBlogComponent } from './return-to-blog/return-to-blog.component';
 
 @NgModule({
   declarations: [
     PageInfoComponent,
     HomePostBlockComponent,
     BlogPostBlockComponent,
+    PostInformationComponent,
+    ReturnToBlogComponent,
   ],
   imports: [CommonModule],
-  exports: [PageInfoComponent, HomePostBlockComponent, BlogPostBlockComponent],
+  exports: [
+    PageInfoComponent,
+    HomePostBlockComponent,
+    BlogPostBlockComponent,
+    PostInformationComponent,
+    ReturnToBlogComponent,
+  ],
 })
 export class SharedComponentsModule {}
