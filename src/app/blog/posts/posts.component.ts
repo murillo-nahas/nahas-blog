3e4bfa77eb0feea8c32d32a07d28ba080a729e92
murillo.nahas <murillonahas@hotmail.com>
2023-01-23 13:05:03 -0300

    feat: removing html files

--- src/app/blog/posts/posts.component.ts
+++ src/app/blog/posts/posts.component.ts
@@ -2,7 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-posts',
-  templateUrl: './posts.component.html',
+  template: `
+    <main class="container">
+      <page-description
+        postTitle="Posts"
+        postDescription="All the posts I created"></page-description>
+      <section class="page-content">
+        <blog-post-block
+          date="Sat, 14/01/2023"
+          postTitle="Hello World"
+          postDescription="This is the first post of my blog. Here I just want to say what I want to share with you and what are my goals for 2023."
+          routerLink="/blog/hello-world"></blog-post-block>
+      </section>
+    </main>
+  `,
   styleUrls: ['./posts.component.scss'],
 })
 export class PostsComponent {}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,7 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
+  template: `<main class="container">
+    <page-description
+      postTitle="Latest"
+      postDescription="My recent stuff"></page-description>
+    <section class="page-content">
+      <home-post-block
+        imagePath="../../../assets/images/posts/hello-world.png"
+        postTitle="Hello World"
+        postDescription="This is the first post of my blog. Here I just want to say what I want to
+      share with you and what are my goals for 2023."
+        routerLink="/blog/hello-world"></home-post-block>
+    </section>
+  </main> `,
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {}
